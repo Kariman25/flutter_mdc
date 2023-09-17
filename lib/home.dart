2e9990fdc33a930b05215a2df09fd267ac41ae87
kariman25 <karimanmahmoud848@gmail.com>
2023-09-17 14:56:12 +0300

    flutter

---
+++ lib/home.dart
@@ -0,0 +1,142 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Homepage());
+}
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/product.dart';
+import 'model/products_repository.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+
+  // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(
+      count,
+          (int index) {
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18.0 / 11.0,
+                child: Image.asset('assets/diamond.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Text('Title'),
+                    SizedBox(height: 8.0),
+                    Text('Secondary Text'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+    return cards;
+  }
+
+
+
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        appBar: AppBar(
+          // TODO: Add buttons and title (102)
+          leading: IconButton(
+            icon: const Icon(
+              Icons.menu,
+              semanticLabel: 'menu',
+            ),
+            onPressed: () {
+              print('Menu button');
+            },
+          ),
+
+          title: const Text('SHRINE'),
+          // TODO: Add trailing buttons (102)
+          // TODO: Add trailing buttons (102)
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.search,
+                semanticLabel: 'search',
+              ),
+              onPressed: () {
+                print('Search button');
+              },
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.tune,
+                semanticLabel: 'filter',
+              ),
+              onPressed: () {
+                print('Filter button');
+              },
+            ),
+          ],
+    ),
+
+      // TODO: Add a grid view (102)
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(10) // Replace
+
+        // TODO: Build a grid of cards (102)
+        children: <Widget>[Card(
+
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 18.0 / 11.0,
+                child: Image.asset('assets/diamond.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Title'),
+                    const SizedBox(height: 8.0),
+                    Text('Secondary Text'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+
+
+        )],
+      ),
+
+
+
+    );
+  }
+}
